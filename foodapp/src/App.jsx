c4b9d252250bf081b3084e0bb399eba57170d0e8
nishkarsh <nishkarshs5@gmail.com>
2024-05-06 02:51:11 +0530

    first session

---
+++ foodapp/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-blue font-primary">
+        This is starting point of food app
+      </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ foodapp/src/layout/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <>
+      <nav>Main</nav>
+      <footer>Footer</footer>
+    </>
+  );
+};
+
+export default Main;
