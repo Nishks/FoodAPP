2c340af563db4dfaec3f2bb3079e13446073fa7a
nishkarsh <nishkarshs5@gmail.com>
2024-06-01 00:19:09 +0530

    Navbar and Banner started

--- foodapp/src/layout/Main.jsx
+++ foodapp/src/layout/Main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../Components/Navbar";
+import "../App.css";
 
 const Main = () => {
   return (
     <>
-      <nav>Main</nav>
+      <Navbar />
+      <Outlet />
       <footer>Footer</footer>
     </>
   );
